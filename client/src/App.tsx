aff5b98602f0f7a6ee623a11538b6d52ec7a4065
donwung <113479299+donwung@users.noreply.github.com>
2023-04-04 12:46:43 -0700

    array type set to accept dictionary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
   const [budget, setBudget] = useState(0);
   const [availableMoney, setAvailableMoney] = useState(0);
-  const [bills, setBills] = useState([10, 20, 30]);
+  const [bills, setBills] = useState([{ name: "netflix", amount: 10 }, { name: "hulu", amount: 20 }, { name: "amazon prime", amount: 30 }]); // array of objects
 
 
   // every budget update, bills gets subtracted from budget
@@ -16,11 +16,16 @@ function App() {
   useEffect(() => {
     let newBudget = budget;
     bills.map((oneBill) => {
-      newBudget -= oneBill;
+      newBudget -= oneBill.amount;
     })
     console.log("Updated budget: " + newBudget);
     setAvailableMoney(newBudget);
   }, [budget])
+
+  const handleOnBillUpdate = () => {
+    // input will receive an id and update that id in API
+    console.log("updating one bill");
+  }
 
   return (
     <div className="App">
@@ -37,6 +42,16 @@ function App() {
         }}></input></h1>
 
       <h1>AvailableMoney: {availableMoney}</h1>
+      <h1>Bills:</h1>
+      <input placeholder='new bill'></input>
+      {bills.map((bill) => {
+        return (
+          <div>
+            {bill.name} : $<input type="number" value={bill.amount} onChange={() => { handleOnBillUpdate() }}></input>
+
+          </div>
+        )
+      })}
 
 
     </div>
